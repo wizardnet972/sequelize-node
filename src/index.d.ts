1c1701fcf02a1e931e9095360662a67d06224ff3
wizardnet972 <wizardnet972@gmail.com>
2018-10-28 09:09:47 +0200

    remove js files

--- src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import * as SequelizeJS from 'sequelize';
-declare let sequelize: SequelizeJS.Sequelize;
-declare const Sequelize: (database: string, username: string, password: string, options: any) => SequelizeJS.Sequelize;
-export { sequelize, Sequelize };
